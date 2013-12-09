483976e05e6a18b97a5b2e6e1b9c0bb0e4c9e991
odorsey <odorsey@live.unc.edu>
2013-12-09 11:45:32 -0500

    Newly re-factored code for intro.py -- yet to be tested!

--- intro.py
+++ intro.py
@@ -11,22 +11,26 @@ response=raw_input("What is your choice?")#Input to ask the user what his or her
 
 if (response).lower() in genderchoices[0]:
   	print " Oh, you're a girl! You are a bar maid with an attitude. After recently being homeless on the streets of Karflooglesville, you are now working for the royal family. In particular, your duty is to serve the king his favorite alcoholic beverages. Today, the King has gotten sick of your beer-serving abilities. For some reason, you are off your game and he insists that the beverage is simply too flat, it tastes unbearable, and the smell is too sweet for his senses." #As a female, this is the backstory that will print out
-    	answer = raw_input("Do you speak up or comply?\n")#Input to ask the user how he/she will answer the king
-    	if 'Speak up' in answer: #If the user speaks up and stands up to the king...
+    	player_input(prompt="Do you speak up or comply?" choice1="speak up", choice2="comply")#Input to ask the user how he/she will answer the king
+    	
+    	answerchoices=['speak up', 'comply']
+    	response= raw_input("What is your choice?") #raw input for response
+    	if (response).lower() in answerchoices[0]: #If the user speaks up and stands up to the king...
 		print "In return, you inform the King that his breath is unbearable and you are sent to the dungeon. The 		King goes into a rage and send you to solitary confinement."#After speaking up, this is the 					response that will be printed out
 		import barmaid_level1.py #After being sent to the dungeon, Level 1 of the Bar Maid's version of the game 		will be imported 
-	if 'Comply' in answer: #If the user complies and does not defy the king...
+	if (response).lower() in answerchoices[1]: #If the user complies and does not defy the king...
 		print "Really? You will let such a defiant moment pass?" #After complying, this is the response that 			will be printed out
 		import intro.py #After complying, the user will be forced to restart the "Intro" portion of the game
-
-	
-if 'Male' in gender: #If the user is a male...
+if (response).lower() in genderchoices[1]:
         print " Oh, a boy I see. So you are a palace scribe who has been working for the King of Karflooglesville for a number of years now. As a young boy, your parents sent you to work for the palace in order to pay for their overdue taxes. The King has grown fond of you, but as of late, the King has been displeased with the notes you've taken because you haven't written enough about him to satisfy his arrogance.\n" #As a male, this is the backstory that will print out
-	answer = raw_input("Do you speak up or comply?\n") #Input to ask the user how he/she will answer the king
-	if 'Speak up' in answer: #If the user speaks up and stands up to the king...
+	player_input(prompt="Do you speak up or comply?" choice1="speak up", choice2="comply")#Input to ask the user how he/she will answer the king
+    	
+    	talkchoices=['speak up', 'comply']
+    	response= raw_input("What is your choice?")#raw input for response
+	if (response).lower() in talkchoices[0]: #If the user speaks up and stands up to the king...
 		print " The King goes into a rage and send you to solitary confinement." #After speaking up, this the 			response that will be printed out
 		import scribe_level1.py #After being sent to the dungeon, Level 1 of the Scribe's version of the game 			will be imported
-	if 'Comply' in answer: #If the user complies and does not defy the king...
+	if (reponse).lower() in talkchoices[1]: #If the user complies and does not defy the king...
 		print "Really? You will let such a defiant moment pass?" #After complying, this is the response that 			will be printed out
 		import intro.py #After complying, the user will be forced to restart the "Intro" portion of the game
 
