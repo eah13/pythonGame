986c8818833a9bd25e25de313c88baeb31dd1e1d
odorsey <odorsey@live.unc.edu>
2013-12-10 14:52:48 -0500

    Added some commas that were missing

--- intro.py
+++ intro.py
@@ -5,13 +5,13 @@ def player_input(prompt, choice1, choice2):
         print "Choice 2: ", choice2;
         
 print "Welcome to our Medieval Python RPG!" 
-player_input(prompt="Are you going to be a male or female in this game?" choice1="female", choice2="male")
+player_input(prompt="Are you going to be a male or female in this game?", choice1="female", choice2="male")
 genderchoices=['female', 'male']
 response=raw_input("What is your choice?")#Input to ask the user what his or her gender is
 
 if (response).lower() in genderchoices[0]:
   	print " Oh, you're a girl! You are a bar maid with an attitude. After recently being homeless on the streets of Karflooglesville, you are now working for the royal family. In particular, your duty is to serve the king his favorite alcoholic beverages. Today, the King has gotten sick of your beer-serving abilities. For some reason, you are off your game and he insists that the beverage is simply too flat, it tastes unbearable, and the smell is too sweet for his senses." #As a female, this is the backstory that will print out
-    	player_input(prompt="Do you speak up or comply?" choice1="speak up", choice2="comply")#Input to ask the user how he/she will answer the king
+    	player_input(prompt="Do you speak up or comply?", choice1="speak up", choice2="comply")#Input to ask the user how he/she will answer the king
     	
     	answerchoices=['speak up', 'comply']
     	response= raw_input("What is your choice?") #raw input for response
@@ -23,7 +23,7 @@ if (response).lower() in genderchoices[0]:
 		import intro.py #After complying, the user will be forced to restart the "Intro" portion of the game
 if (response).lower() in genderchoices[1]:
         print " Oh, a boy I see. So you are a palace scribe who has been working for the King of Karflooglesville for a number of years now. As a young boy, your parents sent you to work for the palace in order to pay for their overdue taxes. The King has grown fond of you, but as of late, the King has been displeased with the notes you've taken because you haven't written enough about him to satisfy his arrogance.\n" #As a male, this is the backstory that will print out
-	player_input(prompt="Do you speak up or comply?" choice1="speak up", choice2="comply")#Input to ask the user how he/she will answer the king
+	player_input(prompt="Do you speak up or comply?", choice1="speak up", choice2="comply")#Input to ask the user how he/she will answer the king
     	
     	talkchoices=['speak up', 'comply']
     	response= raw_input("What is your choice?")#raw input for response
