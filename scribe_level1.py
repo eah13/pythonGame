093bf5c09df4bf771cb22fe1f8f7978d528a3a4e
odorsey <odorsey@live.unc.edu>
2013-12-10 15:29:53 -0500

    Update scribe_level1.py

--- scribe_level1.py
+++ scribe_level1.py
@@ -12,7 +12,7 @@ print " "
 
 player_input(prompt="What will you do in this dank and lonely cell? Break out or stay dank and lonely?", choice1="stay dank and lonely", choice2="break out") #Input asking the user if he/she wants to break out of the cell (and thus play the game) or not 
 cellchoices=['stay dank and lonely', 'break out'] #choices dictionary for either styaing in cell or breaking out of it 
-response= raw_input("What is your choice?") #raw input for response
+response= raw_input("What is your choice? ") #raw input for response
 if (response).lower() in cellchoices[0]:
   print "If you wish..." #Then this is the response that will print
   import scribe_level1.py #And as a result, the user will be forced to the start of the scribe's version of level 1
@@ -21,12 +21,12 @@ if (response).lower() in cellchoices[1]:
   print "Excellent! You take out your handy dandy quill and begin lockpicking the door.\n" #Then the user has the ability   to lockpick the door 
   player_input(prompt="Do you turn your quill to the right or the left first?", choice1="left", choice2="right") #Input asking which way the user wants   to turn the quill to lockpick
   lockchoices= ['left', 'right']
-  response= raw_input("What is your choice?") #raw input for response
+  response= raw_input("What is your choice? ") #raw input for response
   if (response).lower() in lockchoices[0]: #If the user turns it to the left
       print "The lock breaks free and your cell door swings open!" #Then the user is able to break free from the cell 
       player_input(prompt="Which door do you choose to leave from? Door one or door two?", choice1="one", choice2="two") #Input asking the user which door to   leave from
       doorchoices= ['one', 'two']
-      response= raw_input("What is your choice?") #raw input for response 
+      response= raw_input("What is your choice? ") #raw input for response 
       if (response).lower() in doorchoices[0]: #If the user chooses door one...
         print "Guards find you! You are manhandled and forced back into your cell.\n" #The user is forced back into his/her cell
         import scribe_level1.py #Then the user is forced to start the Scribe's version of level 1 over again
@@ -38,7 +38,7 @@ if (response).lower() in cellchoices[1]:
       print "The lock does not budge. Try again.\n" #Then the user is not able to open the door
       player_input(prompt="Do you turn your quill to the right or the left first?", choice1="left", choice2="right") #Input prompting the user to choose       which way to turn the quill to lockpick again
       newlockchoices=['left', 'right']
-      response= raw_input("What is your choice?") #raw input for response
+      response= raw_input("What is your choice? ") #raw input for response
       if (response).lower() in newlockchoices[0]: #If the user turns it to the left
           print "The lock breaks free and your cell door swings open!" #Then the user is able to break free from the cell
       if (response).lower() in newlockchoices[1]: #If the user turns it to the right...
