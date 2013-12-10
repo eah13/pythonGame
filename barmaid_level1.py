157825507fd9ca6b1fe67ded6e514e711a9dfae2
odorsey <odorsey@live.unc.edu>
2013-12-10 15:19:22 -0500

    Added spacing after "What is your choice?"

--- barmaid_level1.py
+++ barmaid_level1.py
@@ -10,7 +10,7 @@ print " "
 print "As a result of your actions, you’re sent to the dungeon to think about what you've done.\n"
 player_input(prompt="What will you do in this dank and lonely cell? Stay dank and lonely or break out?", choice1="stay dank and lonely", choice2="break out")
 cellchoices=['stay dank and lonely', 'break out'] #choices dictionary for either leaving cell or staying in the cell 
-response= raw_input("What is your choice") #raw input for response
+response= raw_input("What is your choice? ") #raw input for response
 
 if (response).lower() in cellchoices[0]:#User chooses to stay dank
 	print "If you wish..."
@@ -20,7 +20,7 @@ if (response).lower() in cellchoices[1]:#User chooses to break out
 
 player_input(prompt="...But how will you break out? The way I see it, you have two options. Break down the cell door with your highly reliable beer mug or break the conveniently located window right behind you?", choice1="break down cell door", choice2="break window")
 breakoutchoices=['break down cell door', 'break window']#choices dictionary for breaking out
-response= raw_input("What is your choice?")#raw input for breaking out of cell response
+response= raw_input("What is your choice? ")#raw input for breaking out of cell response
 if (response).lower() in breakoutchoices[0]:#User chooses to break down cell door
 	print "Alas, to no avail."
 if (response).lower() in breakoutchoices[1]:#User chooses to break window
@@ -28,7 +28,7 @@ if (response).lower() in breakoutchoices[1]:#User chooses to break window
   		
 	player_input(prompt="Now that you've dusted off shards of glass while remaining unharmed, where will you go? To the nearby forest, or back to the palace?", choice1="forest", choice2="palace")
  	freedomchoices=['forest', 'palace'] #choices dictionary for locations
-	response= raw_input("What is your choice?") #raw input for leaving cell and moving to next location
+	response= raw_input("What is your choice? ") #raw input for leaving cell and moving to next location
   	if (response).lower() in freedomchoices[0]: #User chooses to go to the forest
   			print "Onward to the next level!" 
   			import barmaid_level2.py #next level loads
@@ -36,7 +36,7 @@ if (response).lower() in breakoutchoices[1]:#User chooses to break window
   		print "What are you doing?! You can't go back there, the King'll have your head for sure!"
   		player_input(prompt="Do you really want to go back?", choice1="yes", choice2="no")
   		gobackchoices=['yes', 'no'] #choices dictionary for going back to palace or not
-  		response= raw_input("What is your choice?")#raw input for going back to the palace or not 
+  		response= raw_input("What is your choice? ")#raw input for going back to the palace or not 
   		if (response).lower() in gobackchoices[0]: #if the user chooses to go back to the palace...
   			print "The King spots you sneaking across the grounds while he is on one of his evening strolls. He is merciful and sends you back to the dungeon."
   			import barmaid_level1.py #restarts this level
@@ -44,7 +44,7 @@ if (response).lower() in breakoutchoices[1]:#User chooses to break window
   			print "Good. I knew you were wiser than that."
     			player_input(prompt="Now that you've dusted off shards of glass while remaining unharmed, where will you go? To the nearby forest, or back to the palace?", choice1="forest", choice2="palace")
  			freedomchoices=['forest', 'palace'] #choices dictionary for locations
-			response= raw_input("What is your choice?") #raw input for leaving cell and moving to next location
+			response= raw_input("What is your choice? ") #raw input for leaving cell and moving to next location
   			if (response).lower() in freedomchoices[0]: #User chooses to go to the forest
   				print "Onward to the next level!"
   				import barmaid_level2.py #next level loads
